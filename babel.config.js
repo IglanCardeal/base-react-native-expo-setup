8d25279a41bf253b40d5f151aefb3eaef320f636
IglanCardeal <cmtcardeal@outlook.com>
2024-04-12 21:35:15 -0300

    chore: add module alias and editor config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@src': './src',
+            '@test': './test',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@config': './src/config',
+            '@hooks': './src/hooks',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@styles': './src/styles',
+            '@utils': './src/utils'
+          }
+        }
+      ]
+    ]
+  }
+}
